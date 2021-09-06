06deb4be2d6f08f1e7ad4521cb549dba6c67c175
Anshul Raheja <40492015+anshulraheja@users.noreply.github.com>
2021-09-07 01:08:04 +0530

    fix bug & add styling

--- src/components/CashRegister.js
+++ src/components/CashRegister.js
@@ -1,28 +1,54 @@
 import { useState } from "react";
+import { AiOutlineGithub, AiOutlineTwitter } from "react-icons/ai";
+import { FaDiscord, FaLinkedin } from "react-icons/fa";
 
+// variable to store input and output values
 const initialValues = {
-  billAmount: 0,
-  cash: 0
+  billAmount: "",
+  cash: ""
 };
 
 export default function CashRegister() {
+  //state variable track changes in the values
   const [values, setValues] = useState(initialValues);
+  const [check, setCheck] = useState("");
+  const [output, setOutput] = useState([]);
+  const [message, setMessage] = useState("");
 
+  //store input values in state variable
   function handleInputChange(event) {
     const { name, value } = event.target;
     setValues({
       ...values,
-      [name]: value
+      [name]: Number(value)
     });
   }
+
+  const handleCheck = () => {
+    if (values.billAmount === "") {
+      setMessage("Enter the amount to proceed");
+      setCheck(false);
+    } else {
+      setCheck(true);
+    }
+  };
+  //demonination array
   const notes = [2000, 500, 100, 20, 10, 5, 1];
-  let numberOfNotes = Array(7).fill(0);
+
+  //output array - no of notes needed
+  let numberOfNotes = [0, 0, 0, 0, 0, 0, 0];
+
   var returnedAmount = values.cash - values.billAmount;
+
+  //function to calculate no of notes to be returned of each denomination
   function calculateChange() {
     if (parseInt(values.billAmount) <= 0 || parseInt(values.cash) <= 0) {
-      console.log("Bill Amount should be greater than zero");
+      setMessage("Bill Amount should be greater than zero");
     } else if (parseInt(values.billAmount) > parseInt(values.cash)) {
-      console.log("cash should be greater then bill amount");
+      setMessage("cash should be greater then bill amount");
+    } else if (values.billAmount === "") {
+      setMessage("Enter the field values");
+      console.log(message);
     } else {
       for (let i = 0; i < notes.length; i++) {
         if (returnedAmount >= notes[i]) {
@@ -30,26 +56,110 @@ export default function CashRegister() {
           returnedAmount = returnedAmount - numberOfNotes[i] * notes[i];
         }
       }
-      console.log(numberOfNotes);
+      setOutput(numberOfNotes);
     }
   }
   return (
-    <div>
-      <input
-        type="number"
-        placeholder="Enter the bill amount"
-        onChange={handleInputChange}
-        name="billAmount"
-        value={values.billAmount}
-      />
-      <input
-        type="number"
-        placeholder="Enter the cash given"
-        name="cash"
-        value={values.cash}
-        onChange={handleInputChange}
-      />
-      <button onClick={calculateChange}>Calculate Change</button>
+    <div className="container">
+      <h1> Cash Register App</h1>
+      <div className="input-group">
+        <label htmlFor="billAmount">Bill Amount:</label>
+        <input
+          type="number"
+          placeholder="Enter the bill amount"
+          onChange={handleInputChange}
+          name="billAmount"
+        />
+      </div>
+      {check === false ? (
+        <>
+          <button onClick={handleCheck} className="btn btn-check">
+            Check
+          </button>
+          <p className="error-msg">{message}</p>
+        </>
+      ) : (
+        <button onClick={handleCheck} className="btn btn-check">
+          Check
+        </button>
+      )}
+      {check === true ? (
+        <>
+          <div className="input-group">
+            <label htmlFor="billAmount">Cash Given:</label>
+            <input
+              type="number"
+              placeholder="Enter the cash given"
+              name="cash"
+              onChange={handleInputChange}
+            />
+            <button onClick={calculateChange} className="btn calculate-btn">
+              Calculate Change
+            </button>
+          </div>
+          <div className="output-container">
+            <table>
+              <caption>Return Change</caption>
+              <tbody>
+                <tr>
+                  <th>Note</th>
+                  {notes.map((note) => (
+                    <td key={note}>{note}</td>
+                  ))}
+                </tr>
+                <tr>
+                  <th>No. of Notes</th>
+                  {output.map((n, index) => (
+                    <td key={index}>{n}</td>
+                  ))}
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </>
+      ) : null}
+
+      {/*footer  */}
+      <footer>
+        <ul className="footer-list">
+          <li>
+            <a
+              href="https://github.com/anshulraheja"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/anshul-raheja/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://discord.com/channels/anshulraheja#3744"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaDiscord />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/anshulrahejaa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineTwitter />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
